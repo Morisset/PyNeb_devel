3421190bd4b4df95a1e438c8a52cf4feaeeb8fd4
morisset <chris.morisset@gmail.com>
2017-08-18 10:56:41 +0200

    changing upload to pypi using twine

--- updatePyNeb.py
+++ updatePyNeb.py
@@ -16,6 +16,10 @@ md.run_doxygen('doxy_conf_devel.txt')
 md.create_zipfile('pyneb/docs/html', '../../../dist/PyNeb_{0}_documentation'.format(pn.__version__))
 
 #md.run('python setup.py sdist upload')
+
+#md.run('python setup.py sdist bdist_wheel')
+#md.run('twine upload dist/*')
+
 md.run('python setup.py sdist')
 
 md.run('scp dist/PyNeb-{0}.tar.gz taranis:public_html/PyNeb/dist/PyNeb'.format(pn.__version__))
