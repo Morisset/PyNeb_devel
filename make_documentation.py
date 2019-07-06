f75e4eceaaf9c355e499f6e31ed4dea78cc43de3
morisset <chris.morisset@gmail.com>
2019-07-06 12:50:33 -0700

    space and TAB

--- make_documentation.py
+++ make_documentation.py
@@ -14,16 +14,16 @@ def run(command):
 		
 def run_doxygen(config_file):
 	run("sed 's/PROJECT_NUMBER         = .*/PROJECT_NUMBER         = {1}/1' {0} > {0}.tmp".format(config_file, pyneb.__version__))
-        run("mv -f {0}.tmp {0}".format(config_file))
-        run("doxygen {0}".format(config_file))
+	run("mv -f {0}.tmp {0}".format(config_file))
+	run("doxygen {0}".format(config_file))
 
 def make_latex(dir_, manual_name):
 	run("cd {0}; make".format(dir_))
-        run("mv -f {0}/refman.pdf {1}".format(dir_, manual_name))
+	run("mv -f {0}/refman.pdf {1}".format(dir_, manual_name))
 
     
 def create_zipfile(upload_dir, zip_name):
-        run("cd {0}; zip -r --exclude=*.svn* {1}.zip .".format(upload_dir, zip_name))
+    run("cd {0}; zip -r --exclude=*.svn* {1}.zip .".format(upload_dir, zip_name))
 
 def HowTo_inPDF():
 	# Obsolete
