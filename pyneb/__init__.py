ee9074e6062423b6a98fa60933d77e583082a92b
Christophe Morisset <chris.morisset@gmail.com>
2024-01-30 14:09:32 -0600

    correct wrong order in import sequence

--- pyneb/__init__.py
+++ pyneb/__init__.py
@@ -1,15 +1,22 @@
 """
 PyNeb - python package for the analysis of emission lines
 """
-
+"""
+PyNeb - python package for the analysis of emission lines
+"""
 import sys
-from .utils.Config import _Config
 
 from .version import __version__
-from .core.pynebcore import Atom, RecAtom, getAtomDict, getRecEmissivity, EmissionLine, Observation, \
-    parseLineLabel, isValid
-#from .core.pipeline import PipeLine
-from .core.icf import ICF
+from .utils.Config import _Config
+config = _Config()
+log_ = config.log_
+from .utils.manage_atomic_data import _ManageAtomicData
+atomicData = _ManageAtomicData()
+atomicData.defaultDict = 'PYNEB_23_01'
+atomicData.resetDataFileDict()
+from .core.pynebcore import Atom, RecAtom, getAtomDict, getRecEmissivity
+from .core.pynebcore import EmissionLine, Observation, parseLineLabel, isValid
+from .core.icf import ICF 
 from .core.diags import Diagnostics, diags_dict
 from .core.emisGrid import EmisGrid, getEmisGridDict
 from .core.continuum import Continuum
@@ -20,7 +27,6 @@ from .utils.init import LINE_LABEL_LIST, BLEND_LIST, label2levelDict
 from .utils.manage_atomic_data import getLevelsNIST
 from .utils.misc import ROOT_DIR
 from .extinction.red_corr import RedCorr
-from .utils.manage_atomic_data import _ManageAtomicData
 
 __all__ = ['Atom', 'RecAtom', 'getAtomDict', 'getRecEmissivity', 'EmissionLine', 
            'Observation', 'parseLineLabel', 'isValid',
@@ -37,22 +43,15 @@ __all__ = ['Atom', 'RecAtom', 'getAtomDict', 'getRecEmissivity', 'EmissionLine',
 
 __pyversion__ = sys.version_info[0]
 
-config = _Config()
-log_ = config.log_
-log_.message('Starting PyNeb version {}'.format(__version__), calling='PyNeb')
-
-config.import_ai4neb() # Better use a submodule for AI facilities
+log_.message('Starting PyNeb version %s' % __version__, calling='PyNeb')
 
 if sys.version_info[:2] < (2, 6):
     log_.warn('Python version >= 2.6 needed, seems you have {0}'.format(sys.version_info), calling='PyNeb')
 
-atomicData = _ManageAtomicData()
-
-atomicData.defaultDict = 'PYNEB_23_01'
-atomicData.resetDataFileDict()
-
 log_.message('PyNeb ready.', calling='PyNeb')
-log_.timer('Starting PyNeb version {}'.format(__version__), quiet=True, calling='PyNeb')
+log_.timer('Starting PyNeb version %s' % __version__, quiet=True, calling='PyNeb')
+
+config.import_ai4neb()
 
 __info__ = """PyNeb version: {}.
 PyNeb is cited using: Luridiana, V.; Morisset, C.; Shaw, R. A. 2015, A&A, 573, 42
