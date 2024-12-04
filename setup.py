c13a695bcfdc2eb4ea5390cde444b21cb4474a8c
Christophe Morisset <chris.morisset@gmail.com>
2024-12-03 19:17:13 -0600

    setup.py comes back

--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 # To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-from pyneb.version import __version__
 
 setup(name='pyneb', 
-      version=__version__,
-      description='Nebular tools',
-      author='Christophe Morisset, Valentina Luridiana',
-      long_description=long_description,
-      author_email='chris.morisset@gmail.com',
-      url='http://www.iac.es/proyecto/PyNeb/',
-      py_modules=['pyneb.test.test_pyneb'],
       packages=['pyneb',
 		'pyneb.core',
 		'pyneb.plot',
